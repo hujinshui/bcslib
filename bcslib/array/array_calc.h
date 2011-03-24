51f748d9fa695deb433cbdd4cc5de55fe1893322
Dahua Lin <lindahua@gmail.com>
2011-03-24 12:39:55 -0400

    test of array operators passed

--- bcslib/array/array_calc.h
+++ bcslib/array/array_calc.h
@@ -200,9 +200,9 @@ namespace bcs
 
 
 	template<typename T, typename TOrd, class RIndexer0, class RIndexer1, typename TVecFunc>
-	inline array2d<T, TOrd> array_calc(const T& x1, const const_aview2d<T, RIndexer0, RIndexer1>& x2, TVecFunc vecfunc)
+	inline array2d<T, TOrd> array_calc(const T& x1, const const_aview2d<T, TOrd, RIndexer0, RIndexer1>& x2, TVecFunc vecfunc)
 	{
-		array1d<T> y(x2.dim0(), x2.dim1());
+		array2d<T, TOrd> y(x2.dim0(), x2.dim1());
 		size_t n = x2.nelems();
 
 		if (is_dense_view(x2))
@@ -211,10 +211,44 @@ namespace bcs
 		}
 		else
 		{
-			array1d<T> x2c = make_copy(x2);
+			array2d<T, TOrd> x2c = make_copy(x2);
 			vecfunc(n, x1, x2c.pbase(), y.pbase());
 		}
 		return y;
+	}
+
+	template<typename T, class TIndexer, typename TVecFunc>
+	inline void array_calc_inplace(aview1d<T, TIndexer>& x, TVecFunc vecfunc)
+	{
+		size_t n = x.nelems();
+
+		if (is_dense_view(x))
+		{
+			vecfunc(n, x.pbase());
+		}
+		else
+		{
+			array1d<T> xc = make_copy(x);
+			vecfunc(n, xc.pbase());
+			x << xc;
+		}
+	}
+
+	template<typename T, typename TOrd, class TIndexer0, class TIndexer1, typename TVecFunc>
+	inline void array_calc_inplace(aview2d<T, TOrd, TIndexer0, TIndexer1>& x, TVecFunc vecfunc)
+	{
+		size_t n = x.nelems();
+
+		if (is_dense_view(x))
+		{
+			vecfunc(n, x.pbase());
+		}
+		else
+		{
+			array2d<T, TOrd> xc = make_copy(x);
+			vecfunc(n, xc.pbase());
+			x << xc;
+		}
 	}
 
 
@@ -431,6 +465,388 @@ namespace bcs
 	}
 
 
+	// sub
+
+	template<typename T>
+	struct vec_sub_fun
+	{
+		void operator() (size_t n, const T *x1, const T *x2, T *y)
+		{
+			vec_sub(n, x1, x2, y);
+		}
+
+		void operator() (size_t n, const T *x1, const T& x2, T *y)
+		{
+			vec_sub(n, x1, x2, y);
+		}
+
+		void operator() (size_t n, const T& x1, const T *x2, T *y)
+		{
+			vec_sub(n, x1, x2, y);
+		}
+
+		void operator() (size_t n, T *y, const T* x)
+		{
+			vec_sub_inplace(n, y, x);
+		}
+
+		void operator() (size_t n, T *y, const T& x)
+		{
+			vec_sub_inplace(n, y, x);
+		}
+	};
+
+	template<typename T>
+	struct vec_rev_sub_fun
+	{
+		void operator() (size_t n, T *y, const T& x)
+		{
+			vec_sub_inplace(n, x, y);
+		}
+	};
+
+
+	template<typename T, class LIndexer, class RIndexer>
+	array1d<T> operator - (const const_aview1d<T, LIndexer>& x1, const const_aview1d<T, RIndexer>& x2)
+	{
+		return array_calc(x1, x2, vec_sub_fun<T>());
+	}
+
+	template<typename T, typename TOrd, class LIndexer0, class LIndexer1, class RIndexer0, class RIndexer1>
+	array2d<T, TOrd> operator - (
+			const const_aview2d<T, TOrd, LIndexer0, LIndexer1>& x1,
+			const const_aview2d<T, TOrd, RIndexer0, RIndexer1>& x2)
+	{
+		return array_calc(x1, x2, vec_sub_fun<T>());
+	}
+
+	template<typename T, class LIndexer>
+	array1d<T> operator - (const const_aview1d<T, LIndexer>& x1, const T& x2)
+	{
+		return array_calc(x1, x2, vec_sub_fun<T>());
+	}
+
+	template<typename T, typename TOrd, class LIndexer0, class LIndexer1>
+	array2d<T, TOrd> operator - (const const_aview2d<T, TOrd, LIndexer0, LIndexer1>& x1, const T& x2)
+	{
+		return array_calc(x1, x2, vec_sub_fun<T>());
+	}
+
+	template<typename T, class RIndexer>
+	array1d<T> operator - (const T& x1, const const_aview1d<T, RIndexer>& x2)
+	{
+		return array_calc(x1, x2, vec_sub_fun<T>());
+	}
+
+	template<typename T, typename TOrd, class RIndexer0, class RIndexer1>
+	array2d<T, TOrd> operator - (const T& x1, const const_aview2d<T, TOrd, RIndexer0, RIndexer1>& x2)
+	{
+		return array_calc(x1, x2, vec_sub_fun<T>());
+	}
+
+	template<typename T, class YIndexer, class XIndexer>
+	aview1d<T, YIndexer>& operator -= (aview1d<T, YIndexer>& y, const const_aview1d<T, XIndexer>& x)
+	{
+		array_calc_inplace(y, x, vec_sub_fun<T>());
+		return y;
+	}
+
+	template<typename T, typename TOrd, class YIndexer0, class YIndexer1, class XIndexer0, class XIndexer1>
+	aview2d<T, TOrd, YIndexer0, YIndexer1>& operator -= (
+			aview2d<T, TOrd, YIndexer0, YIndexer1>& y,
+			const const_aview2d<T, TOrd, XIndexer0, XIndexer1>& x)
+	{
+		array_calc_inplace(y, x, vec_sub_fun<T>());
+		return y;
+	}
+
+	template<typename T, class YIndexer>
+	aview1d<T, YIndexer>& operator -= (aview1d<T, YIndexer>& y, const T& x)
+	{
+		array_calc_inplace(y, x, vec_sub_fun<T>());
+		return y;
+	}
+
+	template<typename T, typename TOrd, class YIndexer0, class YIndexer1>
+	aview2d<T, TOrd, YIndexer0, YIndexer1>& operator -= (aview2d<T, TOrd, YIndexer0, YIndexer1>& y, const T& x)
+	{
+		array_calc_inplace(y, x, vec_sub_fun<T>());
+		return y;
+	}
+
+
+	template<typename T, class YIndexer>
+	void be_subtracted(const T& x, aview1d<T, YIndexer>& y)
+	{
+		array_calc_inplace(y, x, vec_rev_sub_fun<T>());
+	}
+
+	template<typename T, typename TOrd, class YIndexer0, class YIndexer1>
+	void be_subtracted(const T& x, aview2d<T, TOrd, YIndexer0, YIndexer1>& y)
+	{
+		array_calc_inplace(y, x, vec_rev_sub_fun<T>());
+	}
+
+
+	// mul
+
+	template<typename T>
+	struct vec_mul_fun
+	{
+		void operator() (size_t n, const T *x1, const T *x2, T *y)
+		{
+			vec_mul(n, x1, x2, y);
+		}
+
+		void operator() (size_t n, const T *x1, const T& x2, T *y)
+		{
+			vec_mul(n, x1, x2, y);
+		}
+
+		void operator() (size_t n, T *y, const T* x)
+		{
+			vec_mul_inplace(n, y, x);
+		}
+
+		void operator() (size_t n, T *y, const T& x)
+		{
+			vec_mul_inplace(n, y, x);
+		}
+	};
+
+
+	template<typename T, class LIndexer, class RIndexer>
+	array1d<T> operator * (const const_aview1d<T, LIndexer>& x1, const const_aview1d<T, RIndexer>& x2)
+	{
+		return array_calc(x1, x2, vec_mul_fun<T>());
+	}
+
+	template<typename T, typename TOrd, class LIndexer0, class LIndexer1, class RIndexer0, class RIndexer1>
+	array2d<T, TOrd> operator * (
+			const const_aview2d<T, TOrd, LIndexer0, LIndexer1>& x1,
+			const const_aview2d<T, TOrd, RIndexer0, RIndexer1>& x2)
+	{
+		return array_calc(x1, x2, vec_mul_fun<T>());
+	}
+
+	template<typename T, class LIndexer>
+	array1d<T> operator * (const const_aview1d<T, LIndexer>& x1, const T& x2)
+	{
+		return array_calc(x1, x2, vec_mul_fun<T>());
+	}
+
+	template<typename T, typename TOrd, class LIndexer0, class LIndexer1>
+	array2d<T, TOrd> operator * (const const_aview2d<T, TOrd, LIndexer0, LIndexer1>& x1, const T& x2)
+	{
+		return array_calc(x1, x2, vec_mul_fun<T>());
+	}
+
+	template<typename T, class RIndexer>
+	array1d<T> operator * (const T& x1, const const_aview1d<T, RIndexer>& x2)
+	{
+		return array_calc(x2, x1, vec_mul_fun<T>());
+	}
+
+	template<typename T, typename TOrd, class RIndexer0, class RIndexer1>
+	array2d<T, TOrd> operator * (const T& x1, const const_aview2d<T, TOrd, RIndexer0, RIndexer1>& x2)
+	{
+		return array_calc(x2, x1, vec_mul_fun<T>());
+	}
+
+	template<typename T, class YIndexer, class XIndexer>
+	aview1d<T, YIndexer>& operator *= (aview1d<T, YIndexer>& y, const const_aview1d<T, XIndexer>& x)
+	{
+		array_calc_inplace(y, x, vec_mul_fun<T>());
+		return y;
+	}
+
+	template<typename T, typename TOrd, class YIndexer0, class YIndexer1, class XIndexer0, class XIndexer1>
+	aview2d<T, TOrd, YIndexer0, YIndexer1>& operator *= (
+			aview2d<T, TOrd, YIndexer0, YIndexer1>& y,
+			const const_aview2d<T, TOrd, XIndexer0, XIndexer1>& x)
+	{
+		array_calc_inplace(y, x, vec_mul_fun<T>());
+		return y;
+	}
+
+	template<typename T, class YIndexer>
+	aview1d<T, YIndexer>& operator *= (aview1d<T, YIndexer>& y, const T& x)
+	{
+		array_calc_inplace(y, x, vec_mul_fun<T>());
+		return y;
+	}
+
+	template<typename T, typename TOrd, class YIndexer0, class YIndexer1>
+	aview2d<T, TOrd, YIndexer0, YIndexer1>& operator *= (aview2d<T, TOrd, YIndexer0, YIndexer1>& y, const T& x)
+	{
+		array_calc_inplace(y, x, vec_mul_fun<T>());
+		return y;
+	}
+
+
+	// div
+
+	template<typename T>
+	struct vec_div_fun
+	{
+		void operator() (size_t n, const T *x1, const T *x2, T *y)
+		{
+			vec_div(n, x1, x2, y);
+		}
+
+		void operator() (size_t n, const T *x1, const T& x2, T *y)
+		{
+			vec_div(n, x1, x2, y);
+		}
+
+		void operator() (size_t n, const T& x1, const T *x2, T *y)
+		{
+			vec_div(n, x1, x2, y);
+		}
+
+		void operator() (size_t n, T *y, const T* x)
+		{
+			vec_div_inplace(n, y, x);
+		}
+
+		void operator() (size_t n, T *y, const T& x)
+		{
+			vec_div_inplace(n, y, x);
+		}
+	};
+
+	template<typename T>
+	struct vec_rev_div_fun
+	{
+		void operator() (size_t n, T *y, const T& x)
+		{
+			vec_div_inplace(n, x, y);
+		}
+	};
+
+
+	template<typename T, class LIndexer, class RIndexer>
+	array1d<T> operator / (const const_aview1d<T, LIndexer>& x1, const const_aview1d<T, RIndexer>& x2)
+	{
+		return array_calc(x1, x2, vec_div_fun<T>());
+	}
+
+	template<typename T, typename TOrd, class LIndexer0, class LIndexer1, class RIndexer0, class RIndexer1>
+	array2d<T, TOrd> operator / (
+			const const_aview2d<T, TOrd, LIndexer0, LIndexer1>& x1,
+			const const_aview2d<T, TOrd, RIndexer0, RIndexer1>& x2)
+	{
+		return array_calc(x1, x2, vec_div_fun<T>());
+	}
+
+	template<typename T, class LIndexer>
+	array1d<T> operator / (const const_aview1d<T, LIndexer>& x1, const T& x2)
+	{
+		return array_calc(x1, x2, vec_div_fun<T>());
+	}
+
+	template<typename T, typename TOrd, class LIndexer0, class LIndexer1>
+	array2d<T, TOrd> operator / (const const_aview2d<T, TOrd, LIndexer0, LIndexer1>& x1, const T& x2)
+	{
+		return array_calc(x1, x2, vec_div_fun<T>());
+	}
+
+	template<typename T, class RIndexer>
+	array1d<T> operator / (const T& x1, const const_aview1d<T, RIndexer>& x2)
+	{
+		return array_calc(x1, x2, vec_div_fun<T>());
+	}
+
+	template<typename T, typename TOrd, class RIndexer0, class RIndexer1>
+	array2d<T, TOrd> operator / (const T& x1, const const_aview2d<T, TOrd, RIndexer0, RIndexer1>& x2)
+	{
+		return array_calc(x1, x2, vec_div_fun<T>());
+	}
+
+	template<typename T, class YIndexer, class XIndexer>
+	aview1d<T, YIndexer>& operator /= (aview1d<T, YIndexer>& y, const const_aview1d<T, XIndexer>& x)
+	{
+		array_calc_inplace(y, x, vec_div_fun<T>());
+		return y;
+	}
+
+	template<typename T, typename TOrd, class YIndexer0, class YIndexer1, class XIndexer0, class XIndexer1>
+	aview2d<T, TOrd, YIndexer0, YIndexer1>& operator /= (
+			aview2d<T, TOrd, YIndexer0, YIndexer1>& y,
+			const const_aview2d<T, TOrd, XIndexer0, XIndexer1>& x)
+	{
+		array_calc_inplace(y, x, vec_div_fun<T>());
+		return y;
+	}
+
+	template<typename T, class YIndexer>
+	aview1d<T, YIndexer>& operator /= (aview1d<T, YIndexer>& y, const T& x)
+	{
+		array_calc_inplace(y, x, vec_div_fun<T>());
+		return y;
+	}
+
+	template<typename T, typename TOrd, class YIndexer0, class YIndexer1>
+	aview2d<T, TOrd, YIndexer0, YIndexer1>& operator /= (aview2d<T, TOrd, YIndexer0, YIndexer1>& y, const T& x)
+	{
+		array_calc_inplace(y, x, vec_div_fun<T>());
+		return y;
+	}
+
+
+	template<typename T, class YIndexer>
+	void be_divided(const T& x, aview1d<T, YIndexer>& y)
+	{
+		array_calc_inplace(y, x, vec_rev_div_fun<T>());
+	}
+
+	template<typename T, typename TOrd, class YIndexer0, class YIndexer1>
+	void be_divided(const T& x, aview2d<T, TOrd, YIndexer0, YIndexer1>& y)
+	{
+		array_calc_inplace(y, x, vec_rev_div_fun<T>());
+	}
+
+
+	// negate
+
+	template<typename T>
+	struct vec_neg_fun
+	{
+		void operator() (size_t n, const T *x, T *y)
+		{
+			vec_negate(n, x, y);
+		}
+
+		void operator() (size_t n, T *x)
+		{
+			vec_negate(n, x);
+		}
+	};
+
+
+	template<typename T, class TIndexer>
+	array1d<T> operator - (const const_aview1d<T, TIndexer>& x)
+	{
+		return array_calc(x, vec_neg_fun<T>());
+	}
+
+	template<typename T, typename TOrd, class TIndexer0, class TIndexer1>
+	array2d<T, TOrd> operator - (const const_aview2d<T, TOrd, TIndexer0, TIndexer1>& x)
+	{
+		return array_calc(x, vec_neg_fun<T>());
+	}
+
+	template<typename T, class TIndexer>
+	void be_negated(aview1d<T, TIndexer>& x)
+	{
+		array_calc_inplace(x, vec_neg_fun<T>());
+	}
+
+	template<typename T, typename TOrd, class TIndexer0, class TIndexer1>
+	void be_negated(aview2d<T, TOrd, TIndexer0, TIndexer1>& x)
+	{
+		array_calc_inplace(x, vec_neg_fun<T>());
+	}
 
 
 }
--- test/test_array_calc.cpp
+++ test/test_array_calc.cpp
@@ -160,12 +160,240 @@ BCS_TEST_CASE( test_array_add )
 }
 
 
+BCS_TEST_CASE( test_array_sub )
+{
+	double src1[] = {1, 3, 5, 9, 2, 7};
+	double src2[] = {5, 6, 2, 4, 8, 1};
+
+	// 1D
+
+	array1d<double> x1(5, src1);
+	array1d<double> x2(5, src2);
+
+	double ra1[] = {-4, -3, 3, 5, -6};
+	BCS_CHECK( array_view_approx(x1 - x2, ra1, 5) );
+
+	double ra2[] = {-1, 1, 3, 7, 0};
+	BCS_CHECK( array_view_approx(x1 - 2.0, ra2, 5) );
+
+	double ra3[] = {1, -1, -3, -7, 0};
+	BCS_CHECK( array_view_approx(2.0 - x1, ra3, 5) );
+
+	array1d<double> y1(5);
+
+	y1 << x1;
+	y1 -= x2;
+	BCS_CHECK( array_view_approx(y1, ra1, 5) );
+
+	y1 << x1;
+	y1 -= 2.0;
+	BCS_CHECK( array_view_approx(y1, ra2, 5) );
+
+	y1 << x1;
+	be_subtracted(2.0, y1);
+	BCS_CHECK( array_view_approx(y1, ra3, 5) );
+
+	// 2D
+
+	array2d<double, row_major_t> X1(2, 3, src1);
+	array2d<double, row_major_t> X2(2, 3, src2);
+
+	double rb1[] = {-4, -3, 3, 5, -6, 6};
+	BCS_CHECK( array_view_approx(X1 - X2, rb1, 2, 3) );
+
+	double rb2[] = {-1, 1, 3, 7, 0, 5};
+	BCS_CHECK( array_view_approx(X1 - 2.0, rb2, 2, 3) );
+
+	double rb3[] = {1, -1, -3, -7, 0, -5};
+	BCS_CHECK( array_view_approx(2.0 - X1, rb3, 2, 3) );
+
+	array2d<double, row_major_t> Y1(2, 3);
+
+	Y1 << X1;
+	Y1 -= X2;
+	BCS_CHECK( array_view_approx(Y1, rb1, 2, 3) );
+
+	Y1 << X1;
+	Y1 -= 2.0;
+	BCS_CHECK( array_view_approx(Y1, rb2, 2, 3) );
+
+	Y1 << X1;
+	be_subtracted(2.0, Y1);
+	BCS_CHECK( array_view_approx(Y1, rb3, 2, 3) );
+}
+
+
+BCS_TEST_CASE( test_array_mul )
+{
+	double src1[] = {1, 3, 5, 9, 2, 7};
+	double src2[] = {5, 6, 2, 4, 8, 1};
+
+	// 1D
+
+	array1d<double> x1(5, src1);
+	array1d<double> x2(5, src2);
+
+	double ra1[] = {5, 18, 10, 36, 16};
+	BCS_CHECK( array_view_approx(x1 * x2, ra1, 5) );
+
+	double ra2[] = {2, 6, 10, 18, 4};
+	BCS_CHECK( array_view_approx(x1 * 2.0, ra2, 5) );
+	BCS_CHECK( array_view_approx(2.0 * x1, ra2, 5) );
+
+	array1d<double> y1(5);
+
+	y1 << x1;
+	y1 *= x2;
+	BCS_CHECK( array_view_approx(y1, ra1, 5) );
+
+	y1 << x1;
+	y1 *= 2.0;
+	BCS_CHECK( array_view_approx(y1, ra2, 5) );
+
+	// 2D
+
+	array2d<double, row_major_t> X1(2, 3, src1);
+	array2d<double, row_major_t> X2(2, 3, src2);
+
+	double rb1[] = {5, 18, 10, 36, 16, 7};
+	BCS_CHECK( array_view_approx(X1 * X2, rb1, 2, 3) );
+
+	double rb2[] = {2, 6, 10, 18, 4, 14};
+	BCS_CHECK( array_view_approx(X1 * 2.0, rb2, 2, 3) );
+	BCS_CHECK( array_view_approx(2.0 * X1, rb2, 2, 3) );
+
+	array2d<double, row_major_t> Y1(2, 3);
+
+	Y1 << X1;
+	Y1 *= X2;
+	BCS_CHECK( array_view_approx(Y1, rb1, 2, 3) );
+
+	Y1 << X1;
+	Y1 *= 2.0;
+	BCS_CHECK( array_view_approx(Y1, rb2, 2, 3) );
+}
+
+
+
+BCS_TEST_CASE( test_array_div )
+{
+	double src1[] = {1, 3, 5, 9, 2, 7};
+	double src2[] = {5, 6, 2, 4, 8, 1};
+
+	// 1D
+
+	array1d<double> x1(5, src1);
+	array1d<double> x2(5, src2);
+
+	double ra1[] = {0.2, 0.5, 2.5, 2.25, 0.25};
+	BCS_CHECK( array_view_approx(x1 / x2, ra1, 5) );
+
+	double ra2[] = {0.5, 1.5, 2.5, 4.5, 1.0};
+	BCS_CHECK( array_view_approx(x1 / 2.0, ra2, 5) );
+
+	double ra3[] = {2, 2.0/3, 0.4, 2.0/9, 1.0};
+	BCS_CHECK( array_view_approx(2.0 / x1, ra3, 5) );
+
+	array1d<double> y1(5);
+
+	y1 << x1;
+	y1 /= x2;
+	BCS_CHECK( array_view_approx(y1, ra1, 5) );
+
+	y1 << x1;
+	y1 /= 2.0;
+	BCS_CHECK( array_view_approx(y1, ra2, 5) );
+
+	y1 << x1;
+	be_divided(2.0, y1);
+	BCS_CHECK( array_view_approx(y1, ra3, 5) );
+
+	// 2D
+
+	array2d<double, row_major_t> X1(2, 3, src1);
+	array2d<double, row_major_t> X2(2, 3, src2);
+
+	double rb1[] = {0.2, 0.5, 2.5, 2.25, 0.25, 7.0};
+	BCS_CHECK( array_view_approx(X1 / X2, rb1, 2, 3) );
+
+	double rb2[] = {0.5, 1.5, 2.5, 4.5, 1.0, 3.5};
+	BCS_CHECK( array_view_approx(X1 / 2.0, rb2, 2, 3) );
+
+	double rb3[] = {2, 2.0/3, 0.4, 2.0/9, 1.0, 2.0/7};
+	BCS_CHECK( array_view_approx(2.0 / X1, rb3, 2, 3) );
+
+	array2d<double, row_major_t> Y1(2, 3);
+
+	Y1 << X1;
+	Y1 /= X2;
+	BCS_CHECK( array_view_approx(Y1, rb1, 2, 3) );
+
+	Y1 << X1;
+	Y1 /= 2.0;
+	BCS_CHECK( array_view_approx(Y1, rb2, 2, 3) );
+
+	Y1 << X1;
+	be_divided(2.0, Y1);
+	BCS_CHECK( array_view_approx(Y1, rb3, 2, 3) );
+}
+
+
+BCS_TEST_CASE( test_array_neg )
+{
+	double src1[] = {1, 3, 5, 9, 2, 7, 8, 5, 1};
+
+	// 1D
+
+	array1d<double> x1(5, src1);
+	const_aview1d<double, step_ind> xs1(src1, step_ind(5, 2));
+
+	double ra1[] = {-1, -3, -5, -9, -2};
+	BCS_CHECK( array_view_approx(-x1, ra1, 5) );
+	double ra2[] = {-1, -5, -2, -8, -1};
+	BCS_CHECK( array_view_approx(-xs1, ra2, 5) );
+
+	array1d<double> y1(5, src1);
+	be_negated(y1);
+	BCS_CHECK( array_view_approx(y1, ra1, 5) );
+
+	double y2_buf[10];
+	aview1d<double, step_ind> y2(y2_buf, step_ind(5, 2));
+	y2 << x1;
+	be_negated(y2);
+	BCS_CHECK( array_view_approx(y2, ra1, 5) );
+
+	// 2D
+
+	array2d<double, row_major_t> X1(2, 3, src1);
+	const_aview2d<double, row_major_t, step_ind, id_ind> Xs1(src1, 3, 3, step_ind(2, 2), id_ind(3));
+
+	double rb1[] = {-1, -3, -5, -9, -2, -7};
+	BCS_CHECK( array_view_approx(-X1, rb1, 2, 3) );
+	double rb2[] = {-1, -3, -5, -8, -5, -1};
+	BCS_CHECK( array_view_approx(-Xs1, rb2, 2, 3) );
+
+	array2d<double, row_major_t> Y1(2, 3, src1);
+	be_negated(Y1);
+	BCS_CHECK( array_view_approx(Y1, rb1, 2, 3) );
+
+	double Y2_buf[10];
+	aview2d<double, row_major_t, step_ind, id_ind> Y2(Y2_buf, 3, 3, step_ind(2, 2), id_ind(3));
+	Y2 << X1;
+	be_negated(Y2);
+	BCS_CHECK( array_view_approx(Y2, rb1, 2, 3) );
+
+}
+
 
 test_suite *test_array_calc_suite()
 {
 	test_suite *suite = new test_suite( "test_array_calc" );
 
 	suite->add( new test_array_add() );
+	suite->add( new test_array_sub() );
+	suite->add( new test_array_mul() );
+	suite->add( new test_array_div() );
+	suite->add( new test_array_neg() );
 
 	return suite;
 }
