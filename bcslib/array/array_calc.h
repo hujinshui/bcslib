fdd5d4199a14574a36307276b561aebaaa39101d
Dahua Lin <lindahua@gmail.com>
2011-06-19 18:09:41 -0400

    array arithmetic tested

--- bcslib/array/array_calc.h
+++ bcslib/array/array_calc.h
@@ -175,6 +175,172 @@ namespace bcs
 		return lhs;
 	}
 
+
+
+	// multiplication
+
+	// 1D
+
+	template<typename T, class LIndexer, class RIndexer>
+	inline array1d<T> operator * (const aview1d<T, LIndexer>& lhs, const aview1d<T, RIndexer>& rhs)
+	{
+		return mul_arr_arr(lhs, rhs);
+	}
+
+	template<typename T, class LIndexer>
+	inline array1d<T> operator * (const aview1d<T, LIndexer>& lhs, const T& rhs)
+	{
+		return mul_arr_sca(lhs, rhs);
+	}
+
+	template<typename T, class RIndexer>
+	inline array1d<T> operator * (const T& lhs, const aview1d<T, RIndexer>& rhs)
+	{
+		return mul_arr_sca(rhs, lhs);
+	}
+
+	template<typename T, class LIndexer, class RIndexer>
+	inline aview1d<T, LIndexer>& operator *= (aview1d<T, LIndexer>& lhs, const aview1d<T, RIndexer>& rhs)
+	{
+		mul_arr_arr_inplace(lhs, rhs);
+		return lhs;
+	}
+
+	template<typename T, class LIndexer>
+	inline aview1d<T, LIndexer>& operator *= (aview1d<T, LIndexer>& lhs, const T& rhs)
+	{
+		mul_arr_sca_inplace(lhs, rhs);
+		return lhs;
+	}
+
+	// 2D
+
+	template<typename T, typename TOrd, class LIndexer0, class LIndexer1, class RIndexer0, class RIndexer1>
+	inline array2d<T, TOrd> operator * (
+			const aview2d<T, TOrd, LIndexer0, LIndexer1>& lhs,
+			const aview2d<T, TOrd, RIndexer0, RIndexer1>& rhs)
+	{
+		return mul_arr_arr(lhs, rhs);
+	}
+
+	template<typename T, typename TOrd, class LIndexer0, class LIndexer1>
+	inline array2d<T, TOrd> operator * (const aview2d<T, TOrd, LIndexer0, LIndexer1>& lhs, const T& rhs)
+	{
+		return mul_arr_sca(lhs, rhs);
+	}
+
+	template<typename T, typename TOrd, class RIndexer0, class RIndexer1>
+	inline array2d<T, TOrd> operator * (const T& lhs, const aview2d<T, TOrd, RIndexer0, RIndexer1>& rhs)
+	{
+		return mul_arr_sca(rhs, lhs);
+	}
+
+	template<typename T, typename TOrd, class LIndexer0, class LIndexer1, class RIndexer0, class RIndexer1>
+	inline aview2d<T, TOrd, LIndexer0, LIndexer1>& operator *= (
+			aview2d<T, TOrd, LIndexer0, LIndexer1>& lhs,
+			const aview2d<T, TOrd, RIndexer0, RIndexer1>& rhs)
+	{
+		mul_arr_arr_inplace(lhs, rhs);
+		return lhs;
+	}
+
+	template<typename T, typename TOrd, class LIndexer0, class LIndexer1>
+	inline aview2d<T, TOrd, LIndexer0, LIndexer1>& operator *= (aview2d<T, TOrd, LIndexer0, LIndexer1>& lhs, const T& rhs)
+	{
+		mul_arr_sca_inplace(lhs, rhs);
+		return lhs;
+	}
+
+
+	// division
+
+	// 1D
+
+	template<typename T, class LIndexer, class RIndexer>
+	inline array1d<T> operator / (const aview1d<T, LIndexer>& lhs, const aview1d<T, RIndexer>& rhs)
+	{
+		return div_arr_arr(lhs, rhs);
+	}
+
+	template<typename T, class LIndexer>
+	inline array1d<T> operator / (const aview1d<T, LIndexer>& lhs, const T& rhs)
+	{
+		return div_arr_sca(lhs, rhs);
+	}
+
+	template<typename T, class LIndexer>
+	inline array1d<T> operator / (const T& lhs, const aview1d<T, LIndexer>& rhs)
+	{
+		return div_sca_arr(lhs, rhs);
+	}
+
+	template<typename T, class LIndexer, class RIndexer>
+	inline aview1d<T, LIndexer>& operator /= (aview1d<T, LIndexer>& lhs, const aview1d<T, RIndexer>& rhs)
+	{
+		div_arr_arr_inplace(lhs, rhs);
+		return lhs;
+	}
+
+	template<typename T, class LIndexer>
+	inline aview1d<T, LIndexer>& operator /= (aview1d<T, LIndexer>& lhs, const T& rhs)
+	{
+		div_arr_sca_inplace(lhs, rhs);
+		return lhs;
+	}
+
+	// 2D
+
+	template<typename T, typename TOrd, class LIndexer0, class LIndexer1, class RIndexer0, class RIndexer1>
+	inline array2d<T, TOrd> operator / (
+			const aview2d<T, TOrd, LIndexer0, LIndexer1>& lhs,
+			const aview2d<T, TOrd, RIndexer0, RIndexer1>& rhs)
+	{
+		return div_arr_arr(lhs, rhs);
+	}
+
+	template<typename T, typename TOrd, class LIndexer0, class LIndexer1>
+	inline array2d<T, TOrd> operator / (const aview2d<T, TOrd, LIndexer0, LIndexer1>& lhs, const T& rhs)
+	{
+		return div_arr_sca(lhs, rhs);
+	}
+
+	template<typename T, typename TOrd, class RIndexer0, class RIndexer1>
+	inline array2d<T, TOrd> operator / (const T& lhs, const aview2d<T, TOrd, RIndexer0, RIndexer1>& rhs)
+	{
+		return div_sca_arr(lhs, rhs);
+	}
+
+	template<typename T, typename TOrd, class LIndexer0, class LIndexer1, class RIndexer0, class RIndexer1>
+	inline aview2d<T, TOrd, LIndexer0, LIndexer1>& operator /= (
+			aview2d<T, TOrd, LIndexer0, LIndexer1>& lhs,
+			const aview2d<T, TOrd, RIndexer0, RIndexer1>& rhs)
+	{
+		div_arr_arr_inplace(lhs, rhs);
+		return lhs;
+	}
+
+	template<typename T, typename TOrd, class LIndexer0, class LIndexer1>
+	inline aview2d<T, TOrd, LIndexer0, LIndexer1>& operator /= (aview2d<T, TOrd, LIndexer0, LIndexer1>& lhs, const T& rhs)
+	{
+		div_arr_sca_inplace(lhs, rhs);
+		return lhs;
+	}
+
+
+	// negation
+
+	template<typename T, class RIndexer>
+	inline array1d<T> operator - (const aview1d<T, RIndexer>& rhs)
+	{
+		return neg_arr(rhs);
+	}
+
+	template<typename T, typename TOrd, class RIndexer0, class RIndexer1>
+	inline array2d<T, TOrd> operator - (const aview2d<T, TOrd, RIndexer0, RIndexer1>& rhs)
+	{
+		return neg_arr(rhs);
+	}
+
 }
 
 #endif 
--- bcslib/veccomp/veccalc_functors.h
+++ bcslib/veccomp/veccalc_functors.h
@@ -17,34 +17,34 @@ namespace bcs
 	// addition
 
 	template<typename T>
-	struct vec_vec_add
+	struct vec_vec_add_ftor
 	{
 		typedef T result_value_type;
 		void operator() (size_t n, const T *x1, const T *x2, T *y) const { vec_add(n, x1, x2, y); }
 	};
 
 	template<typename T>
-	struct vec_sca_add
+	struct vec_sca_add_ftor
 	{
 		T s;
-		vec_sca_add(const T& s_) : s(s_) { }
+		vec_sca_add_ftor(const T& s_) : s(s_) { }
 
 		typedef T result_value_type;
 		void operator() (size_t n, const T *x1, T *y) const { vec_add(n, x1, s, y); }
 	};
 
 	template<typename T>
-	struct vec_vec_add_ip
+	struct vec_vec_add_ip_ftor
 	{
 		typedef T result_value_type;
 		void operator() (size_t n, T *y, const T *x) const { vec_add_inplace(n, y, x); }
 	};
 
 	template<typename T>
-	struct vec_sca_add_ip
+	struct vec_sca_add_ip_ftor
 	{
 		T s;
-		vec_sca_add_ip(const T& s_) : s(s_) { }
+		vec_sca_add_ip_ftor(const T& s_) : s(s_) { }
 
 		typedef T result_value_type;
 		void operator() (size_t n, T *y) const { vec_add_inplace(n, y, s); }
@@ -54,50 +54,150 @@ namespace bcs
 	// subtraction
 
 	template<typename T>
-	struct vec_vec_sub
+	struct vec_vec_sub_ftor
 	{
 		typedef T result_value_type;
 		void operator() (size_t n, const T *x1, const T *x2, T *y) const { vec_sub(n, x1, x2, y); }
 	};
 
 	template<typename T>
-	struct vec_sca_sub
+	struct vec_sca_sub_ftor
 	{
 		T s;
-		vec_sca_sub(const T& s_) : s(s_) { }
+		vec_sca_sub_ftor(const T& s_) : s(s_) { }
 
 		typedef T result_value_type;
 		void operator() (size_t n, const T *x1, T *y) const { vec_sub(n, x1, s, y); }
 	};
 
 	template<typename T>
-	struct sca_vec_sub
+	struct sca_vec_sub_ftor
 	{
 		T s;
-		sca_vec_sub(const T& s_) : s(s_) { }
+		sca_vec_sub_ftor(const T& s_) : s(s_) { }
 
 		typedef T result_value_type;
 		void operator() (size_t n, const T *x2, T *y) const { vec_sub(n, s, x2, y); }
 	};
 
 	template<typename T>
-	struct vec_vec_sub_ip
+	struct vec_vec_sub_ip_ftor
 	{
 		typedef T result_value_type;
 		void operator() (size_t n, T *y, const T *x) const { vec_sub_inplace(n, y, x); }
 	};
 
 	template<typename T>
-	struct vec_sca_sub_ip
+	struct vec_sca_sub_ip_ftor
 	{
 		T s;
-		vec_sca_sub_ip(const T& s_) : s(s_) { }
+		vec_sca_sub_ip_ftor(const T& s_) : s(s_) { }
 
 		typedef T result_value_type;
 		void operator() (size_t n, T *y) const { vec_sub_inplace(n, y, s); }
 	};
 
 
+	// multiplication
+
+	template<typename T>
+	struct vec_vec_mul_ftor
+	{
+		typedef T result_value_type;
+		void operator() (size_t n, const T *x1, const T *x2, T *y) const { vec_mul(n, x1, x2, y); }
+	};
+
+	template<typename T>
+	struct vec_sca_mul_ftor
+	{
+		T s;
+		vec_sca_mul_ftor(const T& s_) : s(s_) { }
+
+		typedef T result_value_type;
+		void operator() (size_t n, const T *x1, T *y) const { vec_mul(n, x1, s, y); }
+	};
+
+	template<typename T>
+	struct vec_vec_mul_ip_ftor
+	{
+		typedef T result_value_type;
+		void operator() (size_t n, T *y, const T *x) const { vec_mul_inplace(n, y, x); }
+	};
+
+	template<typename T>
+	struct vec_sca_mul_ip_ftor
+	{
+		T s;
+		vec_sca_mul_ip_ftor(const T& s_) : s(s_) { }
+
+		typedef T result_value_type;
+		void operator() (size_t n, T *y) const { vec_mul_inplace(n, y, s); }
+	};
+
+
+	// division
+
+	template<typename T>
+	struct vec_vec_div_ftor
+	{
+		typedef T result_value_type;
+		void operator() (size_t n, const T *x1, const T *x2, T *y) const { vec_div(n, x1, x2, y); }
+	};
+
+	template<typename T>
+	struct vec_sca_div_ftor
+	{
+		T s;
+		vec_sca_div_ftor(const T& s_) : s(s_) { }
+
+		typedef T result_value_type;
+		void operator() (size_t n, const T *x1, T *y) const { vec_div(n, x1, s, y); }
+	};
+
+	template<typename T>
+	struct sca_vec_div_ftor
+	{
+		T s;
+		sca_vec_div_ftor(const T& s_) : s(s_) { }
+
+		typedef T result_value_type;
+		void operator() (size_t n, const T *x2, T *y) const { vec_div(n, s, x2, y); }
+	};
+
+	template<typename T>
+	struct vec_vec_div_ip_ftor
+	{
+		typedef T result_value_type;
+		void operator() (size_t n, T *y, const T *x) const { vec_div_inplace(n, y, x); }
+	};
+
+	template<typename T>
+	struct vec_sca_div_ip_ftor
+	{
+		T s;
+		vec_sca_div_ip_ftor(const T& s_) : s(s_) { }
+
+		typedef T result_value_type;
+		void operator() (size_t n, T *y) const { vec_div_inplace(n, y, s); }
+	};
+
+
+	// negation
+
+	template<typename T>
+	struct vec_neg_ftor
+	{
+		typedef T result_value_type;
+		void operator() (size_t n, const T *x, T *y) const { vec_negate(n, x, y); }
+	};
+
+
+	template<typename T>
+	struct vec_neg_ip_ftor
+	{
+		typedef T result_value_type;
+		void operator() (size_t n, T *y) const { vec_negate(n, y); }
+	};
 
 }
 
