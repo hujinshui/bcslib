7e07011a4b4e74ba252d2fb77aeb5b3db3b0b8c4
Dahua Lin <lindahua@gmail.com>
2011-06-23 12:21:16 -0400

    min, max, and minmax tested

--- test/test_array_stat.cpp
+++ test/test_array_stat.cpp
@@ -13,6 +13,7 @@
 #include <bcslib/array/array2d.h>
 #include <bcslib/array/array_stat.h>
 
+#include <limits>
 
 using namespace bcs;
 using namespace bcs::test;
@@ -79,6 +80,55 @@ struct dmean
 	void put(double x) { s += x; ++n; }
 	double get() const { return s / (double)n; }
 };
+
+
+struct dmin
+{
+	typedef double result_type;
+
+	double s;
+
+	dmin() : s(std::numeric_limits<double>::infinity()) { }
+	void put(double x) { if (x < s) s = x; }
+	double get() const { return s; }
+};
+
+
+struct dmax
+{
+	typedef double result_type;
+
+	double s;
+
+	dmax() : s(-std::numeric_limits<double>::infinity()) { }
+	void put(double x) { if (x > s) s = x; }
+	double get() const { return s; }
+};
+
+
+struct dminmax
+{
+	typedef std::pair<double, double> result_type;
+
+	bool started;
+	std::pair<double, double> s;
+
+	dminmax()
+	{
+		s.first = std::numeric_limits<double>::infinity();
+		s.second = -std::numeric_limits<double>::infinity();
+	}
+
+	void put(double x)
+	{
+		if (x < s.first) s.first = x;
+		if (x > s.second) s.second = x;
+	}
+
+	result_type get() const { return s; }
+};
+
+
 
 
 BCS_TEST_CASE( test_sum_dot_and_mean )
@@ -212,11 +262,99 @@ BCS_TEST_CASE( test_sum_dot_and_mean )
 }
 
 
+BCS_TEST_CASE( test_min_and_max )
+{
+	const size_t Nmax = 36;
+	double src[Nmax] = {
+			3, 1, 8, 4, 6, 2,
+			2, 5, 7, 3, 1, 9,
+			4, 3, 6, 9, 2, 8,
+			9, 4, 2, 7, 3, 1,
+			1, 7, 9, 2, 8, 4,
+			5, 6, 3, 8, 2, 7
+	};
+
+	// prepare views
+
+	aview1d<double> v1d = get_aview1d(src, 6);
+	aview1d<double, step_ind> v1d_s = get_aview1d_ex(src, step_ind(6, 2));
+
+	aview2d<double, row_major_t>    v2d_rm = get_aview2d_rm(src, 2, 3);
+	aview2d<double, column_major_t> v2d_cm = get_aview2d_cm(src, 2, 3);
+
+	aview2d<double, row_major_t,    step_ind, step_ind> v2d_rm_s = get_aview2d_rm_ex(src, 4, 6, step_ind(2, 2), step_ind(3, 2));
+	aview2d<double, column_major_t, step_ind, step_ind> v2d_cm_s = get_aview2d_cm_ex(src, 4, 6, step_ind(2, 2), step_ind(3, 2));
+
+
+	// min
+
+	BCS_CHECK_EQUAL( min(v1d), accum_all(v1d, dmin()) );
+	BCS_CHECK_EQUAL( min(v2d_rm), accum_all(v2d_rm, dmin()) );
+	BCS_CHECK_EQUAL( min(v2d_cm), accum_all(v2d_cm, dmin()) );
+
+	BCS_CHECK_EQUAL( min(v2d_rm, per_row()), accum_prow(v2d_rm, dmin()) );
+	BCS_CHECK_EQUAL( min(v2d_cm, per_row()), accum_prow(v2d_cm, dmin()) );
+	BCS_CHECK_EQUAL( min(v2d_rm, per_col()), accum_pcol(v2d_rm, dmin()) );
+	BCS_CHECK_EQUAL( min(v2d_cm, per_col()), accum_pcol(v2d_cm, dmin()) );
+
+	BCS_CHECK_EQUAL( min(v1d_s), accum_all(v1d_s, dmin()) );
+	BCS_CHECK_EQUAL( min(v2d_rm_s), accum_all(v2d_rm_s, dmin()) );
+	BCS_CHECK_EQUAL( min(v2d_cm_s), accum_all(v2d_cm_s, dmin()) );
+
+	BCS_CHECK_EQUAL( min(v2d_rm_s, per_row()), accum_prow(v2d_rm_s, dmin()) );
+	BCS_CHECK_EQUAL( min(v2d_cm_s, per_row()), accum_prow(v2d_cm_s, dmin()) );
+	BCS_CHECK_EQUAL( min(v2d_rm_s, per_col()), accum_pcol(v2d_rm_s, dmin()) );
+	BCS_CHECK_EQUAL( min(v2d_cm_s, per_col()), accum_pcol(v2d_cm_s, dmin()) );
+
+	// max
+
+	BCS_CHECK_EQUAL( max(v1d), accum_all(v1d, dmax()) );
+	BCS_CHECK_EQUAL( max(v2d_rm), accum_all(v2d_rm, dmax()) );
+	BCS_CHECK_EQUAL( max(v2d_cm), accum_all(v2d_cm, dmax()) );
+
+	BCS_CHECK_EQUAL( max(v2d_rm, per_row()), accum_prow(v2d_rm, dmax()) );
+	BCS_CHECK_EQUAL( max(v2d_cm, per_row()), accum_prow(v2d_cm, dmax()) );
+	BCS_CHECK_EQUAL( max(v2d_rm, per_col()), accum_pcol(v2d_rm, dmax()) );
+	BCS_CHECK_EQUAL( max(v2d_cm, per_col()), accum_pcol(v2d_cm, dmax()) );
+
+	BCS_CHECK_EQUAL( max(v1d_s), accum_all(v1d_s, dmax()) );
+	BCS_CHECK_EQUAL( max(v2d_rm_s), accum_all(v2d_rm_s, dmax()) );
+	BCS_CHECK_EQUAL( max(v2d_cm_s), accum_all(v2d_cm_s, dmax()) );
+
+	BCS_CHECK_EQUAL( max(v2d_rm_s, per_row()), accum_prow(v2d_rm_s, dmax()) );
+	BCS_CHECK_EQUAL( max(v2d_cm_s, per_row()), accum_prow(v2d_cm_s, dmax()) );
+	BCS_CHECK_EQUAL( max(v2d_rm_s, per_col()), accum_pcol(v2d_rm_s, dmax()) );
+	BCS_CHECK_EQUAL( max(v2d_cm_s, per_col()), accum_pcol(v2d_cm_s, dmax()) );
+
+	// minmax
+
+	BCS_CHECK_EQUAL( minmax(v1d), accum_all(v1d, dminmax()) );
+	BCS_CHECK_EQUAL( minmax(v2d_rm), accum_all(v2d_rm, dminmax()) );
+	BCS_CHECK_EQUAL( minmax(v2d_cm), accum_all(v2d_cm, dminmax()) );
+
+	BCS_CHECK_EQUAL( minmax(v2d_rm, per_row()), accum_prow(v2d_rm, dminmax()) );
+	BCS_CHECK_EQUAL( minmax(v2d_cm, per_row()), accum_prow(v2d_cm, dminmax()) );
+	BCS_CHECK_EQUAL( minmax(v2d_rm, per_col()), accum_pcol(v2d_rm, dminmax()) );
+	BCS_CHECK_EQUAL( minmax(v2d_cm, per_col()), accum_pcol(v2d_cm, dminmax()) );
+
+	BCS_CHECK_EQUAL( minmax(v1d_s), accum_all(v1d_s, dminmax()) );
+	BCS_CHECK_EQUAL( minmax(v2d_rm_s), accum_all(v2d_rm_s, dminmax()) );
+	BCS_CHECK_EQUAL( minmax(v2d_cm_s), accum_all(v2d_cm_s, dminmax()) );
+
+	BCS_CHECK_EQUAL( minmax(v2d_rm_s, per_row()), accum_prow(v2d_rm_s, dminmax()) );
+	BCS_CHECK_EQUAL( minmax(v2d_cm_s, per_row()), accum_prow(v2d_cm_s, dminmax()) );
+	BCS_CHECK_EQUAL( minmax(v2d_rm_s, per_col()), accum_pcol(v2d_rm_s, dminmax()) );
+	BCS_CHECK_EQUAL( minmax(v2d_cm_s, per_col()), accum_pcol(v2d_cm_s, dminmax()) );
+
+}
+
+
 test_suite* test_array_stat_suite()
 {
 	test_suite* suite = new test_suite("test_array_stat");
 
 	suite->add( new test_sum_dot_and_mean() );
+	suite->add( new test_min_and_max() );
 
 	return suite;
 }
