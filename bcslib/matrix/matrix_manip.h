58fe3187294dcd7aaee7bf996637fd04fbb979ef
Dahua Lin <lindahua@gmail.com>
2012-04-29 00:08:37 -0400

    merge matrix_base and matrix_fwd

--- bcslib/matrix/matrix_manip.h
+++ bcslib/matrix/matrix_manip.h
@@ -13,7 +13,7 @@
 #ifndef BCSLIB_MATRIX_MANIP_H_
 #define BCSLIB_MATRIX_MANIP_H_
 
-#include <bcslib/matrix/matrix_fwd.h>
+#include <bcslib/matrix/matrix_base.h>
 #include <bcslib/matrix/bits/matrix_manip_helpers.h>
 #include <cstdio>
 
