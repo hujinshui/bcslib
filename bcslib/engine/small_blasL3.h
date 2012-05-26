02c3f990fc4bad365a2d31cecc832129f3b512d3
Dahua Lin <lindahua@gmail.com>
2012-05-26 00:12:29 -0400

    small gemm_nn tested

--- bcslib/engine/small_blasL3.h
+++ bcslib/engine/small_blasL3.h
@@ -21,7 +21,7 @@ namespace bcs { namespace engine {
 	struct small_gemm_11K  // (1 x K) * (K x 1)
 	{
 		BCS_ENSURE_INLINE
-		void eval_nn_b0(const T alpha,
+		static void eval_nn_b0(const T alpha,
 				const T* __restrict__ a, const int lda,
 				const T* __restrict__ b, const int ldb,
 				T* __restrict__ c, const int ldc)
@@ -37,7 +37,7 @@ namespace bcs { namespace engine {
 		}
 
 		BCS_ENSURE_INLINE
-		void eval_nn_b1(const T alpha,
+		static void eval_nn_b1(const T alpha,
 				const T* __restrict__ a, const int lda,
 				const T* __restrict__ b, const int ldb,
 				T* __restrict__ c, const int ldc)
@@ -58,7 +58,7 @@ namespace bcs { namespace engine {
 	struct small_gemm_1NK  // (1 x K) * (K x N)
 	{
 		BCS_ENSURE_INLINE
-		void eval_nn_b0(const T alpha,
+		static void eval_nn_b0(const T alpha,
 				const T* __restrict__ a, const int lda,
 				const T* __restrict__ b, const int ldb,
 				T* __restrict__ c, const int ldc)
@@ -74,18 +74,18 @@ namespace bcs { namespace engine {
 		}
 
 		BCS_ENSURE_INLINE
-		void eval_nn_b1(const T alpha,
+		static void eval_nn_b1(const T alpha,
 				const T* __restrict__ a, const int lda,
 				const T* __restrict__ b, const int ldb,
 				T* __restrict__ c, const int ldc)
 		{
 			if (lda == 1 && ldc == 1)
 			{
-				small_gemv_n_MN<T, K, N>::eval_b1(alpha, b, ldb, a, c);
+				small_gemv_t_MN<T, K, N>::eval_b1(alpha, b, ldb, a, c);
 			}
 			else
 			{
-				small_gemv_n_MN<T, K, N>::eval_b1(alpha, b, ldb, a, lda, c, ldc);
+				small_gemv_t_MN<T, K, N>::eval_b1(alpha, b, ldb, a, lda, c, ldc);
 			}
 		}
 	};
@@ -95,7 +95,7 @@ namespace bcs { namespace engine {
 	struct small_gemm_M1K  // (M x K) * (K x 1)
 	{
 		BCS_ENSURE_INLINE
-		void eval_nn_b0(const T alpha,
+		static void eval_nn_b0(const T alpha,
 				const T* __restrict__ a, const int lda,
 				const T* __restrict__ b, const int ldb,
 				T* __restrict__ c, const int ldc)
@@ -104,12 +104,12 @@ namespace bcs { namespace engine {
 		}
 
 		BCS_ENSURE_INLINE
-		void eval_nn_b1(const T alpha,
+		static void eval_nn_b1(const T alpha,
 				const T* __restrict__ a, const int lda,
 				const T* __restrict__ b, const int ldb,
 				T* __restrict__ c, const int ldc)
 		{
-			small_gemv_n_MN<T, M, K>::eval_b0(alpha, a, lda, b, c);
+			small_gemv_n_MN<T, M, K>::eval_b1(alpha, a, lda, b, c);
 		}
 	};
 
@@ -118,7 +118,7 @@ namespace bcs { namespace engine {
 	struct small_gemm_MNK  // (M x K) * (K x N)
 	{
 		BCS_ENSURE_INLINE
-		void eval_nn_b0(const T alpha,
+		static void eval_nn_b0(const T alpha,
 				const T* __restrict__ a, const int lda,
 				const T* __restrict__ b, const int ldb,
 				T* __restrict__ c, const int ldc)
@@ -130,7 +130,7 @@ namespace bcs { namespace engine {
 		}
 
 		BCS_ENSURE_INLINE
-		void eval_nn_b1(const T alpha,
+		static void eval_nn_b1(const T alpha,
 				const T* __restrict__ a, const int lda,
 				const T* __restrict__ b, const int ldb,
 				T* __restrict__ c, const int ldc)
@@ -147,7 +147,7 @@ namespace bcs { namespace engine {
 	struct small_gemm_MN1  // (M x 1) * (1 x N)
 	{
 		BCS_ENSURE_INLINE
-		void eval_nn_b0(const T alpha,
+		static void eval_nn_b0(const T alpha,
 				const T* __restrict__ a, const int lda,
 				const T* __restrict__ b, const int ldb,
 				T* __restrict__ c, const int ldc)
@@ -163,7 +163,7 @@ namespace bcs { namespace engine {
 		}
 
 		BCS_ENSURE_INLINE
-		void eval_nn_b1(const T alpha,
+		static void eval_nn_b1(const T alpha,
 				const T* __restrict__ a, const int lda,
 				const T* __restrict__ b, const int ldb,
 				T* __restrict__ c, const int ldc)
@@ -181,7 +181,7 @@ namespace bcs { namespace engine {
 
 
 	template<typename T, int M, int N, int K>
-	struct small_gemm
+	struct small_gemm_ker
 	{
 		typedef typename select_type<M == 1,
 					typename select_type<N == 1,
@@ -198,7 +198,7 @@ namespace bcs { namespace engine {
 				>::type impl_t;
 
 		BCS_ENSURE_INLINE
-		void eval_nn_b0(const T alpha,
+		static void eval_nn_b0(const T alpha,
 				const T* __restrict__ a, const int lda,
 				const T* __restrict__ b, const int ldb,
 				T* __restrict__ c, const int ldc)
@@ -207,7 +207,7 @@ namespace bcs { namespace engine {
 		}
 
 		BCS_ENSURE_INLINE
-		void eval_nn_b1(const T alpha,
+		static void eval_nn_b1(const T alpha,
 				const T* __restrict__ a, const int lda,
 				const T* __restrict__ b, const int ldb,
 				T* __restrict__ c, const int ldc)
@@ -215,6 +215,34 @@ namespace bcs { namespace engine {
 			impl_t::eval_nn_b1(alpha, a, lda, b, ldb, c, ldc);
 		}
 	};
+
+
+	template<typename T, int M, int N, int K>
+	struct small_gemm
+	{
+		static void eval_nn(const T alpha,
+				const T* __restrict__ a, const int lda,
+				const T* __restrict__ b, const int ldb,
+				const T beta,
+				T* __restrict__ c, const int ldc)
+		{
+			if (beta == 0)
+			{
+				small_gemm_ker<T, M, N, K>::eval_nn_b0(alpha, a, lda, b, ldb, c, ldc);
+			}
+			else
+			{
+				if (beta != 1)
+				{
+					for (int j = 0; j < N; ++j) mul_ker<T, M>::eval(beta, c + ldc * j);
+				}
+
+				small_gemm_ker<T, M, N, K>::eval_nn_b1(alpha, a, lda, b, ldb, c, ldc);
+			}
+		}
+
+	};
+
 
 } }
 
